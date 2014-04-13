f23d7e8475c0540a724df0aa132b59f5401ca60a
Retina <Retina@Retinas-MacBook-Pro.local>
2014-04-14 01:02:24 +0200

    Added flags and language selection in GUI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,11 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  
+  def language = Action(parse.urlFormEncoded) { implicit request =>
+    val code = request.body.get("code").head.head
+    Logger.info(s"Langugage set to $code")
+    Ok(s"Language code set to: $code").withCookies(Cookie("languageCode", code))
+  }
 
 }
--- app/utils/Language.scala
+++ app/utils/Language.scala
@@ -7,16 +7,7 @@ import play.api.Play
 trait Language {
   
   private val defaultLanguage = "se"
-  private var language = defaultLanguage
 
-  implicit val lang = Lang.get(language).getOrElse("defaultLanguage")
-  
-  def setLanguage(langCode: String) = {
-    language = langCode
-  }
-  
-  def getLanguage(langCode: String) = {
-    language
-  }
+  implicit val lang = Lang.get(defaultLanguage).getOrElse("defaultLanguage")
   
 }
