79999e2887a478395b520ac73f3a34918240bf83
Retina <Retina@Retinas-MacBook-Pro.local>
2014-04-14 02:17:10 +0200

    Cleaned up logging

--- app/utils/Language.scala
+++ app/utils/Language.scala
@@ -12,11 +12,6 @@ object Language {
   private val defaultLanguage = "se"
 
   def langCode(request: Request[Any]): Lang = {
-    request.session.get("languageCode").map { languageCode =>
-      Logger.info("Found languageCode")
-    }.getOrElse {
-      Logger.info("Did not found languageCode")
-    }
     val lang: Lang = Lang.get(request.session.get("languageCode").getOrElse(defaultLanguage)).getOrElse(Lang(defaultLanguage))
     Logger.info(s"Using langugage code: $lang")
     lang
