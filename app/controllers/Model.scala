165afee5eca6795ad4f759642eb24f6682d0ff14
Retina <m@m.com>
2014-08-06 00:22:53 +0200

    Added Secure Social on the all Action

--- app/controllers/Model.scala
+++ app/controllers/Model.scala
@@ -6,6 +6,8 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.db.slick._
+import play.api.data.validation._
+import play.api.Play.current
 import models.Tag
 import models.Tags
 import utils.FormHelper.saveFormFile
@@ -14,38 +16,38 @@ import org.joda.time.DateTime
 import models.Models
 import models.TagModels
 import models.TagModel
-import play.api.data.validation._
+import scala.slick.driver.PostgresDriver.simple._
 
-object Model extends Controller {
+object Model extends Controller with securesocial.core.SecureSocial {
 
   case class Model(name: String, material: String, location: String, text: String, year: Int, tags: List[Tag])
-  
-  def all = DBAction { implicit request =>
-    val models = Models.all
-    val modelsTags = models.map{ model =>
-      val tags = Tags.tags(model)
+
+  def all = SecuredAction { implicit request =>
+    val models = DB.withSession { implicit session => Models.all }
+    val modelsTags = models.map { model =>
+      val tags = DB.withSession { implicit session => Tags.tags(model) }
       (model, tags)
     }
     Ok(views.html.model.browser(modelsTags))
   }
-  
+
   def thumbnail(id: Int) = DBAction { implicit request =>
     Models.get(id) match {
       case None => NotFound("The requested model is either not in the db or you lack access to it.")
       case Some(model) => {
         Ok(views.html.model.thumbnail(model, Tags.tags(model)))
       }
-    }  
+    }
   }
 
   val formObject = "object-file"
   val textureObject = "texture-file"
   val thumbnailObject = "thumbnail-file"
-    
+
   val fourDigitYearConstraint: Constraint[Int] = Constraint("constraints.4digityear") {
     case i if i > DateTime.now.year.get => Invalid("error.inFuture")
-	case i if i.toString.length == 4 => Valid
-	case _ => Invalid("error.4digityear")
+    case i if i.toString.length == 4 => Valid
+    case _ => Invalid("error.4digityear")
   }
   val fourDigitYearCheck: Mapping[Int] = number.verifying(fourDigitYearConstraint)
 
@@ -75,7 +77,7 @@ object Model extends Controller {
     def addFileMissingErrorsToForm(form: play.api.data.Form[Model], filesMissing: List[(String, String)]): play.api.data.Form[Model] = {
       filesMissing match {
         case Nil => form
-        case head :: tail => addFileMissingErrorsToForm(form.withError(head._1,head._2), tail)
+        case head :: tail => addFileMissingErrorsToForm(form.withError(head._1, head._2), tail)
       }
     }
     modelForm.bindFromRequest.fold(
@@ -86,7 +88,7 @@ object Model extends Controller {
         // TODO: Take userID from session
         val dbModel = new models.Model(id = None, name = m.name, userID = 1, date = new DateTime(m.year, 1, 1, 0, 0, 0),
           material = m.material, location = m.location, text = m.text,
-          pathObject = saveFormFile(request, formObject), 
+          pathObject = saveFormFile(request, formObject),
           pathTexure = saveFormFile(request, textureObject),
           pathThumbnail = saveFormFile(request, thumbnailObject))
         Logger.info(s"model: $dbModel")
